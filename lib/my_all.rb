46537a63bed4c8ef7ea063a3a0ed19757de22b3c
Jesse Wenger <wengerj@gmail.com>
2021-03-03 18:28:13 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,13 +2,8 @@ require 'pry'
 
 def my_all?(collection)
   i = 0 
-  if true
-    while i < collection.length 
-      yield(collection[i])
-      i += 1
-    end
-    collection
-  else
-    false
+  while i < collection.length
+    yield(collection[i])
+    i += 1
   end
 end
